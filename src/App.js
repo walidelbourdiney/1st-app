1e2c0d10c3a66ea0d8f33c4511632e6e67bafe44
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-01-01 20:22:11 +0200

    hello, world!

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
 import "./App.css";
 
 function App() {
+  const audience = "world";
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Hello, {audience}!</p>
       </header>
     </div>
   );
