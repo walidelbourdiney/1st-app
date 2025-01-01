0032fd04bee32b960d962a8c8aeaf085cf9b010a
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-01-01 22:55:34 +0200

    add style to index.css

--- src/Header.js
+++ src/Header.js
@@ -2,14 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header
-      style={{
-        backgroundColor: "mediumblue",
-        color: "#fff",
-        width: "80%",
-        margin: "auto",
-      }}
-    >
+    <header>
       <h1>Groceries List</h1>
     </header>
   );
