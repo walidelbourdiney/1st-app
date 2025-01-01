c0ca241f90f2a5bc965798ede24f847a28f8915b
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-01-01 22:04:39 +0200

    1st function component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 
 function App() {
-  const audience = "world";
+  const audience = ["Walid", "Ali", "World"];
+  const int = Math.floor(Math.random() * 3);
+  const name = audience[int];
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello, {audience}!</p>
+        <p>Hello, {name}!</p>
       </header>
     </div>
   );
