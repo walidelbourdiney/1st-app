6627a75472af937280cf03e60ada2117ca7247c9
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-01-03 19:09:01 +0200

    Shopping  List by react

--- src/Content.js
+++ src/Content.js
@@ -1,25 +1,60 @@
 import React from "react";
 import "./Content.css";
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
-  const [name, setName] = useState("Walid");
-  const [count, setCount] = useState(0);
-  const handleChange = () => {
-    const audience = ["Walid", "Ali", "World"];
-    const int = Math.floor(Math.random() * 3);
-    setName(audience[int]);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Item 1",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
   };
-  const handleCount = () => {
-    setCount(count + 1);
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
   };
 
   return (
     <main>
-      <p>Hello, {name}!</p>
-      <button onClick={handleChange}>Change Name</button>
-      <p>{count}</p>
-      <button onClick={handleCount}>Change count</button>
+      <ul>
+        {items.map((item) => (
+          <li
+            onChange={() => handleCheck(item.id)}
+            className="item"
+            key={item.id}
+          >
+            <input type="checkbox" checked={item.checked} />
+            <label onDoubleClick={() => handleCheck(item.id)}>
+              {item.item}
+            </label>
+            <FaTrashAlt
+              role="button"
+              tabIndex="0"
+              onClick={() => handleDelete(item.id)}
+            />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
