ed10416d15992328cfb2e51b682a39ff880cc0d6
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-01-03 11:06:48 +0200

    add useState hook

--- src/Content.js
+++ src/Content.js
@@ -1,13 +1,20 @@
 import React from "react";
 import "./Content.css";
+import { useState } from "react";
 
 const Content = () => {
-  const audience = ["Walid", "Ali", "World"];
-  const int = Math.floor(Math.random() * 3);
-  const name = audience[int];
+  const [name, setName] = useState("Walid");
+
+  const handleChange = () => {
+    const audience = ["Walid", "Ali", "World"];
+    const int = Math.floor(Math.random() * 3);
+    setName(audience[int]);
+  };
+
   return (
     <main>
       <p>Hello, {name}!</p>
+      <button onClick={handleChange}>Change Name</button>
     </main>
   );
 };
