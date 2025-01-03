a10f20e614592de8940fb94274276ff8e57c806f
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-01-03 11:17:02 +0200

    write the useState properly

--- src/Content.js
+++ src/Content.js
@@ -4,15 +4,14 @@ import { useState } from "react";
 
 const Content = () => {
   const [name, setName] = useState("Walid");
-  let [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
   const handleChange = () => {
     const audience = ["Walid", "Ali", "World"];
     const int = Math.floor(Math.random() * 3);
     setName(audience[int]);
   };
   const handleCount = () => {
-    count++;
-    setCount(count);
+    setCount(count + 1);
   };
 
   return (
