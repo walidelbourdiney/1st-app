4baacd4c6dfba22b2b54d08337ba1a79a735bbba
walidelbourdiney <walidelbourdiney25@gmail.com>
2025-01-04 17:40:54 +0200

    content component props drilling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
+import { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Item 1",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { ...item, checked: !item.checked } : item
+    );
+    setItems(listItems);
+  };
+
+  const handleDelete = (id) => {
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+  };
+
   return (
     <div className="App">
       <Header title="Groceries" />
-      <Content />
+      <Content
+        items={items}
+        setItems={setItems}
+        handleCheck={handleCheck}
+        handleDelete={handleDelete}
+      />
       <Footer />
     </div>
   );
--- src/Content.js
+++ src/Content.js
@@ -1,39 +1,8 @@
 import React from "react";
 import "./Content.css";
-import { useState } from "react";
 import { FaTrashAlt } from "react-icons/fa";
 
-const Content = () => {
-  const [items, setItems] = useState([
-    {
-      id: 1,
-      checked: false,
-      item: "Item 1",
-    },
-    {
-      id: 2,
-      checked: false,
-      item: "Item 2",
-    },
-    {
-      id: 3,
-      checked: false,
-      item: "Item 3",
-    },
-  ]);
-
-  const handleCheck = (id) => {
-    const listItems = items.map((item) =>
-      item.id === id ? { ...item, checked: !item.checked } : item
-    );
-    setItems(listItems);
-  };
-
-  const handleDelete = (id) => {
-    const listItems = items.filter((item) => item.id !== id);
-    setItems(listItems);
-  };
-
+const Content = ({ items, setItems, handleDelete, handleCheck }) => {
   return (
     <main>
       <ul>
